f3e74d3ee318809e7636726fe73716f59dbaea2e
Rodrigo <143302152+rodrigo-barba@users.noreply.github.com>
2024-07-13 23:23:10 -0300

    nueva URL de la API peliculas

--- js/post_pelicula.js
+++ js/post_pelicula.js
@@ -40,7 +40,8 @@ document.addEventListener('DOMContentLoaded', async () => {
             })
         };
         //realizo la peticion fetch a la api para agregar una pelicula
-        const response = await fetch('http://localhost:8080/apisimple/peliculas', options);
+//      const response = await fetch('http://localhost:8080/app/peliculas', options);
+		const response = await fetch('http://localhost/apiPeliculas/peliculas', options);        
         //obtengo la respuesta
         const data = await response.json();
         //si la respuesta es correcta, muestro un mensaje de exito y limpio los inputs del formulario
